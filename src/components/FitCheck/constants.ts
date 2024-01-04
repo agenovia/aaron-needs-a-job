7d6b215d82567c0cec255503459ed378283d66d4
agenovia <aarongenovia@gmail.com>
2024-01-03 19:03:39 -0800

    new placeholder

--- src/components/FitCheck/constants.ts
+++ src/components/FitCheck/constants.ts
@@ -1,4 +1,14 @@
 export const placeholders = {
   textarea: `Got a job description? Type or paste it in here and \
-click the submit button to see if my experience meets your needs.\n\n`,
+click the submit button to see if my experience meets your needs.\n\n\
+For example:\n\
+\t"We are seeking a seasoned Senior Data Analyst with extensive SQL experience to join our dynamic team. If you excel in crafting complex SQL queries, optimizing databases, and leading ETL processes, we want to hear from you. As a leader in our data analytics team, you will leverage your expertise to mentor junior analysts, foster collaboration, and ensure the delivery of high-quality insights. Your strategic mindset will be crucial in translating business requirements into actionable data-driven solutions, providing valuable insights for strategic decision-making.
+
+Key Responsibilities:
+
+Advanced SQL Proficiency: Showcase your expertise in crafting intricate SQL queries, optimizing databases, and leading ETL processes.
+
+Team Leadership: Lead and manage cross-functional teams, mentoring junior analysts, and fostering a collaborative work environment.
+
+Strategic Data Analysis: Translate business requirements into data-driven solutions, delivering actionable insights to support strategic decision-making."\n\n`,
 };
